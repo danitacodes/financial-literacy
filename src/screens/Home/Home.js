dc937d153434a39a47943b648f327865e352812b
Danita Codes <danitacodes@gmail.com>
2022-07-01 20:52:35 -0700

    adding screens and font

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import "./../Home/Home.css"
+
+export default function Home() {
+  return (
+    <div className='main'>
+        <Container>
+            <Row>
+                <div className="intro-text">
+                    <div>
+                        <h1 className='title'>It's never to early to</h1>
+                        <h1 className='subtitle'>Learn</h1>
+                    </div>
+                </div>
+            </Row>
+        </Container>
+    </div>
+  )
+}
+
