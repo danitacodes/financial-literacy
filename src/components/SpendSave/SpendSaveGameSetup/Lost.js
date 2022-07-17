90d297a0a4cd21a7c2a429526b4c1f1c3e5300f8
Danita Codes <danitacodes@gmail.com>
2022-07-16 20:13:30 -0700

    still working on framer motion

--- src/components/SpendSave/SpendSaveGameSetup/Lost.js
+++ src/components/SpendSave/SpendSaveGameSetup/Lost.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 
 const Lost = () => {
   return (
-    <div id="Start">
-        <h1>Sorry that wasn't it</h1>
-        <Link to="/game">Try Again</Link>
+    <div name='lost'>
+          <div className="max-w-[1000px] mx-auto p-12 flex-col justify-center w-full text-center">
+        <div>
+        <h1 className="text-seagreen font-Lato text-5xl font-bold mb-4">Try Again</h1>
+        <Link to="/spend/game" className="font-Lato text-3xl bg-seagreen rounded px-4 text-white hover:bg-[#FFD6BA]">Start Game</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/SpendSave/util/spendSaveConfig.js
+++ src/components/SpendSave/util/spendSaveConfig.js
@@ -1,16 +1,16 @@
 const spendSaveConfig = {
     gameTitle: "Spend or Save?",
     maxErrors: 5,
-    interval: 7,
-    fallingTime: 10,
+    interval: 3,
+    fallingTime: 5,
     itemSize: {
         width: 100,
         height: 100
     },
     bucketSize: {
         width: 600,
-        height: 250,
-        margin: 10
+        height: 200,
+        margin: 5
     }
 }
 
